fd5b13a7252edf09a71bb60bfdd9206b8e01dd75
Marc-Philipp Esser <m-esser@mail.de>
2020-12-09 13:10:25 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Dataviz with Seaborn',
+    version='1.0.0',
+    url='https://github.com/m-p-esser/dataviz-with-seaborn',
+    author='Marc-Philipp Esser',
+    author_email='m-esser@mail.de',
+    description='Description of my package',
+    packages=find_packages()
+)
