a0c6b7ea30e6e65c7852b8c505b7a7e4c3c08fb7
Marc-Philipp Esser <m-esser@mail.de>
2020-12-17 12:05:33 +0100

    Changed variable names

--- dataviz_gallery/crud.py
+++ dataviz_gallery/crud.py
@@ -8,30 +8,47 @@ import pandas as pd
 from dataviz_gallery import models
 
 
-def csv_to_df(file_name: str):
-    data_dir = pathlib.Path.cwd() / 'data' / 'plot_taxanomy'
-    file_path = data_dir / file_name
+def csv_to_df(relative_path: str):
+    file_path = pathlib.Path.cwd() / relative_path
     df = pd.read_csv(file_path, delimiter=';')
+
     return df
 
 
 def df_to_query_list(df: pd.DataFrame):
     query_list = df.to_dict('records')
+
     return query_list
 
 
 def bulk_create(query_list):
     instances = [
         models.Plot(
-            name=entry['Name'],
-            image='img/' + str(entry['Name']).lower().replace(' ', '_') +
+            parent_name=entry['parent_name'],
+            variation_name=entry['variation_name'],
+            synonyms=entry['synonyms'],
+            goals=entry['goals'],
+            dimensionality=entry['dimensionality'],
+            numerical=entry['numerical'],
+            categorical=entry['categorical'],
+            visual_cues=entry['visual_cues'],
+            coordinate_system=entry['coordinate_system'],
+            shapes=entry['shapes'],
+            anatomy=entry['anatomy'],
+            caveats=entry['caveats'],
+            facetted=entry['facetted'],
+            single_facetted=entry['single_facetted'],
+            dual_facetted=entry['dual_facetted'],
+            grouped=entry['grouped'],
+            plot_package=entry['plot_package'],
+            related=entry['related'],
+            implemented=entry['implemented'],
+            icon='img/icon/' + str(entry['parent_name']).lower().replace(' ', '_') +
+            ".png",
+            seaborn='img/seaborn/' + str(entry['variation_name']).lower().replace(' ', '_') +
             ".png",
         ) for entry in query_list
     ]
     models.Plot.objects.bulk_create(instances, ignore_conflicts=True)
 
-
-if __name__ == "__main__":
-    df = csv_to_df('plot_taxanomy.csv')
-    query_list = df_to_query_list(df)
-    bulk_create(query_list)
+    print("Added entries to database table")
