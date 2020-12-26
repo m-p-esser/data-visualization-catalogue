dbaf1e10af54d6ab7a128542b8e09efd36f67780
Marc-Philipp Esser <m-esser@mail.de>
2020-12-27 00:32:50 +0100

    Added crispy forms

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dataviz_gallery'
+    'dataviz_gallery',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
