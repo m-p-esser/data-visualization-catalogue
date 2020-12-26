999aea146fb1ac7495ec4e1a2d85b958c53114a5
Marc-Philipp Esser <m-esser@mail.de>
2020-12-27 00:35:14 +0100

    Added new view to index url

--- dataviz_gallery/urls.py
+++ dataviz_gallery/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from dataviz_gallery import views
 
 urlpatterns = [
-    path("", views.plot_index, name="plot_index"),
+    # path("", views.plot_overview, name="plot_overview"),
     path("<str:pk>/", views.plot_detail, name="plot_detail"),
+    path("", views.PlotListView.as_view(), name="plot_list_view")
 ]
