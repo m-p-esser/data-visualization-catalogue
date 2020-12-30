2764dbc5f19af45c737218b90172aef03a2de18c
Marc-Philipp Esser <m-esser@mail.de>
2020-12-30 20:31:56 +0100

    Added data viz goal website to urls

--- dataviz_gallery/urls.py
+++ dataviz_gallery/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from dataviz_gallery import views
 
 urlpatterns = [
-    # path("", views.plot_overview, name="plot_overview"),
-    path("<str:pk>/", views.plot_detail, name="plot_detail"),
+    path("plot/<str:pk>", views.plot_detail, name="plot_detail"),
+    path("goal/<goal>", views.GoalListView.as_view(), name="plot_goal"),
+    path("shape/<shape>", views.ShapeListView.as_view(), name="plot_shape"),
+    path("search/<q>", views.SearchListView.as_view(), name="plot_search"),
+    path("goals", views.dataviz_goals, name="dataviz_goals"),
     path("", views.PlotListView.as_view(), name="plot_list_view")
 ]
