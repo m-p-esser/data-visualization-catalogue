950d5c6bbf66519b760596b2bb1e0d40d2857609
Marc-Philipp Esser <m-esser@mail.de>
2021-01-16 21:34:10 +0100

    Added functions to save plotly plot as div and create fig from data sample

--- plot/plot_utils.py
+++ plot/plot_utils.py
@@ -1,6 +1,9 @@
 """ Collection of helper functions related to plot module"""
 
 import pathlib
+import plotly
+import plotly.figure_factory as ff
+import pandas as pd
 
 
 def concat_strings(*strings):
@@ -63,10 +66,34 @@ def remove_prefix_from_file_name(prefix, relative_folder_path, file_types=['.png
                 file_extension = p.suffix
                 old_file_name = p.stem
                 new_file_name = old_file_name.replace(prefix, "") + file_extension
-                print(new_file_name)
                 p.rename(pathlib.Path(parent_dir, new_file_name))
             else:
                 print(f"Path {p} doesn't lead to a file or file type {p.suffix} is not in list of allowed file types {file_types}")
         except Exception as e:
             print("Exception occured")
             print(e)
+
+
+def save_plot_as_div(figure, plot_name):
+    """ Write div containing plot to file """
+    # if is_running_from_ipython():
+    #     input_dir = pathlib.Path.cwd().parent.parent.parent / relative_folder_path
+    # else:
+    #     input_dir = pathlib.Path.cwd() / relative_folder_path
+    output_dir = pathlib.Path.cwd().parent.parent.parent / "dataviz_gallery/templates/plotly"
+    file_path = output_dir / f"{plot_name}.html"
+    div = plotly.offline.plot(figure_or_data=figure, include_plotlyjs=False, output_type='div')
+    with open(file_path, "w") as f:
+        f.write(div)
+
+
+def create_table(data_frame, cols, order_cols, sample_size=12):
+    """ Create plotly table which can be embedded on website """
+    data_frame = data_frame[cols]
+    df_sample = data_frame.sample(n=sample_size, random_state=42)
+    if order_cols is not None:
+        df_sample = df_sample.sort_values(by=order_cols)
+    colorscale = [[0, "#292b2c"],[0.5, '#f7f7f7'],[1, '#ffffff']]
+    fig = ff.create_table(df_sample, colorscale=colorscale)
+    fig.show()
+    return fig
